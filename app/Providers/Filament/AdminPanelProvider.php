1254f3fb68c2183e19b4e935ef5b3c5604d1345c
Ivan Muntahir <77213038+ivanmuntahir@users.noreply.github.com>
2025-08-01 14:00:43 +0700

    change id and path of route admin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,8 +25,8 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
-            ->id('admin')
-            ->path('admin')
+            ->id('siap')
+            ->path('siap')
             ->login()
             ->colors([
                 'primary' => Color::Blue,
